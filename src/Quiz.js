453613d4c961aded6e07a24bdd88797e7860abd9
Christopher Culbreath <christopher@moltenlavapit.com>
2020-09-17 18:28:59 -0700

    4b

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,18 +12,13 @@ class Quiz extends Component {
 
   render() {
     const isQuizEnd =
-      this.state.quizposition - 1 == quizData.quiz_questions.length;
+      this.state.quizposition - 1 === quizData.quiz_questions.length;
     return (
       <div>
-        if(isQuizEnd)
-        {
-          <QuizQuestion
-            quiz_question={
-              quizData.quiz_questions[this.state.quiz_position - 1]
-            }
-          />
-        }
-        else {<QuizEnd />}
+        (isQuizEnd) ? <QuizEnd /> :
+        <QuizQuestion
+          quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+        />
       </div>
     );
   }
