48aaaf9bb58cd851cf1e2895883b516172b798ae
Christopher Culbreath <christopher@moltenlavapit.com>
2020-09-18 01:06:42 -0700

    sd

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,14 +4,14 @@ import QuizQuestionButton from "./QuizQuestionButton.js";
 class QuizQuestion extends Component {
   constructor(props) {
     super(props)
-    this.state = {incorrect_answer: false}
+    this.state = {incorrectAnswer: false}
   }
   handleClick(buttonText){
     if (buttonText === this.props.quiz_question.answer) {
-      this.setState({incorrect_answer: false})
+      this.setState({incorrectAnswer: false})
       this.props.showNextQuestionHandler()
   } else {
-    this.setState({incorrect_answer: true})
+    this.setState({incorrectAnswer: true})
   }
 }
   render() {
@@ -25,7 +25,7 @@ class QuizQuestion extends Component {
           {this.props.quiz_question.answer_options.map((val, key) => <QuizQuestionButton clickHandler={this.handleClick.bind(this)} key={key} button_text={val}/>)}
           </ul>
         </section>
-        {(this.state.incorrect_answer) ? <p className="error">Sorry, that's not right</p> : null}
+        {(this.state.incorrectAnswer) ? <p className="error">Sorry, that's not right</p> : null}
       </main>
     );
   }
